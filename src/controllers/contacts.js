463f9af018449509ff0264f06215c9defa65b770
Viktor <viktoryanenko11@gmail.com>
2025-08-08 16:22:43 +0300

    add pagination

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,61 +1,73 @@
-import { getAllContacts, getContactById, createContact, updateContact, deleteContact } from '../services/contacts.js';
+import {
+  getAllContacts,
+  getContactById,
+  createContact,
+  updateContact,
+  deleteContact,
+} from '../services/contacts.js';
 import createHttpError from 'http-errors';
-export const getContactsController = async (req, res) => {
-    const contacts = await getAllContacts();
+import { parsePaginationParams } from '../utils/parsePaginationParams.js';
 
-    res.json({
-        status: 200,
-        message: 'Successfully found contacts!',
-        data: contacts,
-    });
+export const getContactsController = async (req, res) => {
+  const { page, perPage } = parsePaginationParams(req.query);
+  const contacts = await getAllContacts({
+    page,
+    perPage,
+  });
+
+  res.json({
+    status: 200,
+    message: 'Successfully found contacts!',
+    data: contacts,
+  });
 };
 
-export const getContactByIdController = async (req, res,next) => {
-    const { contactId } = req.params;
-    const contact = await getContactById(contactId);
+export const getContactByIdController = async (req, res, next) => {
+  const { contactId } = req.params;
+  const contact = await getContactById(contactId);
 
-    if (!contact) {
-        throw createHttpError(404, 'Contact not found');
-    }
+  if (!contact) {
+    throw createHttpError(404, 'Contact not found');
+  }
 
-    res.status(200).json({
-        status: 200,
-        message: `Successfully found contact with id ${contactId}`,
-        data: contact,
-    });
+  res.status(200).json({
+    status: 200,
+    message: `Successfully found contact with id ${contactId}`,
+    data: contact,
+  });
 };
 
 export const createContactController = async (req, res) => {
-    const contact = await createContact(req.body);
-    res.status(201).json({
-        status: 201,
-        message: 'Successfully created a contact!',
-        data: contact,
-    })
+  const contact = await createContact(req.body);
+  res.status(201).json({
+    status: 201,
+    message: 'Successfully created a contact!',
+    data: contact,
+  });
 };
 
 export const patchContactController = async (req, res, next) => {
-    const { contactId } = req.params;
-    const result = await updateContact(contactId, req.body);
+  const { contactId } = req.params;
+  const result = await updateContact(contactId, req.body);
 
-    if (!result) {
-        next(createHttpError(404, 'Contact not found'));
-        return;
-    }
-    res.json({
-        status: 200,
-        message: 'Successfully patched a contact!',
-        data: result.contact,
-    });
+  if (!result) {
+    next(createHttpError(404, 'Contact not found'));
+    return;
+  }
+  res.json({
+    status: 200,
+    message: 'Successfully patched a contact!',
+    data: result.contact,
+  });
 };
 
 export const deleteContactController = async (req, res, next) => {
-    const { contactId } = req.params;
-    const contact = await deleteContact(contactId);
+  const { contactId } = req.params;
+  const contact = await deleteContact(contactId);
 
-    if (!contact) {
-        next(createHttpError(404, 'Contact not found'));
-        return;
-    }
-    res.status(204).send();
-}
+  if (!contact) {
+    next(createHttpError(404, 'Contact not found'));
+    return;
+  }
+  res.status(204).send();
+};
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,8 +1,23 @@
 import { ContactsCollection } from '../db/models/contact.js';
+import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 
-export const getAllContacts = async () => {
-  const contacts = await ContactsCollection.find();
-  return contacts;
+export const getAllContacts = async ({ page, perPage }) => {
+  const limit = perPage;
+  const skip = (page - 1) * perPage;
+
+  const contactsQuery = ContactsCollection.find();
+  const contactsCount = await ContactsCollection.find()
+    .merge()
+    .countDocuments();
+
+  const contacts = await contactsQuery.skip(skip).limit(limit).exec();
+
+  const paginationData = calculatePaginationData(contactsCount, perPage, page);
+
+  return {
+    data: contacts,
+    ...paginationData,
+  };
 };
 
 export const getContactById = async (contactId) => {
@@ -29,7 +44,7 @@ export const updateContact = async (contactId, payload, options = {}) => {
   return {
     contact: rawResult.value,
     isNew: Boolean(rawResult?.lastErrorObject?.upserted),
-  }
+  };
 };
 
 export const deleteContact = async (contactId) => {
