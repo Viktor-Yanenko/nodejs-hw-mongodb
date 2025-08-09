e2c65bc27f11f29c31eacd196b75eca57a06b677
Viktor <viktoryanenko11@gmail.com>
2025-08-09 18:59:41 +0300

    try do filter on isFavourite

--- src/utils/parseFilterParams.js
+++ src/utils/parseFilterParams.js
@@ -7,12 +7,19 @@ const parseContactType = (contactType) => {
   if (isContactType(contactType)) return contactType;
 };
 
+// const parseIsFavourite = (isFavourite) => {
+//   const isBoolean = typeof isFavourite === 'boolean';
+//   if (isBoolean) return isFavourite;
+// };
+
 export const parseFilterParams = (query) => {
   const { contactType } = query;
 
   const parsedContactType = parseContactType(contactType);
+  //   const parsedIsFavourite = parseIsFavourite(isFavourite);
 
   return {
     contactType: parsedContactType,
+    // isFavourite: parsedIsFavourite,
   };
 };
