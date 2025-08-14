29bcf326c5c8b2da0fb88edbc28eb88fd9374145
Viktor <viktoryanenko11@gmail.com>
2025-08-14 13:58:46 +0300

    upd login

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,15 +1,33 @@
 import { loginUser, registerUser } from '../services/auth.js';
+import { THIRTY_DAYS } from '../constants/index.js';
 
 export const registerUserController = async (req, res) => {
   const user = await registerUser(req.body);
 
   res.status(201).json({
     status: 201,
-    message: 'Successfully register a user!',
+    message: 'Successfully registered a user!',
     data: user,
   });
 };
 
 export const loginUserController = async (req, res) => {
-  await loginUser(req.body);
+  const session = await loginUser(req.body);
+
+  res.cookie('refreshToken', session.refreshToken, {
+    httpOnly: true,
+    expires: new Date(Date.now() + THIRTY_DAYS),
+  });
+  res.cookie('sessionId', session._id, {
+    httpOnly: true,
+    expires: new Date(Date.now() + THIRTY_DAYS),
+  });
+
+  res.json({
+    status: 200,
+    message: 'Successfully logged in an user!',
+    data: {
+      accessToken: session.accessToken,
+    },
+  });
 };
