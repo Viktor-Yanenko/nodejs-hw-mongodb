992eea1b39f1b3bb2b95e4c14e72d2571f11d76d
Viktor <viktoryanenko11@gmail.com>
2025-08-15 17:03:52 +0300

    add middleware

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,4 +1,5 @@
 import Joi from 'joi';
+import { isValidObjectId } from 'mongoose';
 
 export const createContactsSchema = Joi.object({
   name: Joi.string().min(3).max(20).required().messages({
@@ -55,5 +56,11 @@ export const updateContactsSchema = Joi.object({
   contactType: Joi.string().valid('work', 'home', 'personal').messages({
     'string.base': 'Contact type should be a string',
     'string.valid': 'Contact type should be "work", "home" or "personal"',
+  }),
+  userId: Joi.string().custom((value, helper) => {
+    if (value && !isValidObjectId(value)) {
+      return helper.message('User id should be a valid mongo id');
+    }
+    return true;
   }),
 });
